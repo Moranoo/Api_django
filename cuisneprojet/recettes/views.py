b6d7ca8eb41d6c418f5a215b5ad155258f8369f9
Mourad <20512086-12mourad4@users.noreply.gitlab.com>
2024-06-11 18:01:45 +0200

    Front React V2

--- cuisneprojet/recettes/views.py
+++ cuisneprojet/recettes/views.py
@@ -1,7 +1,22 @@
-from rest_framework import viewsets
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework.pagination import PageNumberPagination
 from .models import Recipe
 from .serializers import RecipeSerializer
 
-class RecipeViewSet(viewsets.ModelViewSet):
-    queryset = Recipe.objects.all()
-    serializer_class = RecipeSerializer
+class RecipePagination(PageNumberPagination):
+    page_size = 10
+
+@api_view(['GET'])
+def get_recipes(request):
+    recipes = Recipe.objects.all()
+    paginator = RecipePagination()
+    result_page = paginator.paginate_queryset(recipes, request)
+    serializer = RecipeSerializer(result_page, many=True)
+    return paginator.get_paginated_response(serializer.data)
+
+@api_view(['GET'])
+def get_ingredients(request):
+    ingredients = Ingredient.objects.all()
+    serializer = IngredientSerializer(ingredients, many=True)
+    return Response(serializer.data)
