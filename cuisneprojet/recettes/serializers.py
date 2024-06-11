5f91179c6fb6d2105f3fad3c541e29e524b6e532
DavalEnzo <davalenzo@gmail.com>
2024-06-11 17:37:33 +0200

    modification et ajout de serializers pour rendre la requête complètes

--- cuisneprojet/recettes/serializers.py
+++ cuisneprojet/recettes/serializers.py
@@ -1,7 +1,22 @@
 from rest_framework import serializers
-from .models import Recipe
+from .models import Recipe, RecipeIngredient, Ingredient
+
+
+class IngredientSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Ingredient
+        fields = ['name']
+
+class RecipeIngredientSerializer(serializers.ModelSerializer):
+    name = serializers.CharField(source='ingredient.name')
+
+    class Meta:
+        model = RecipeIngredient
+        fields = ['name', 'quantity']
 
 class RecipeSerializer(serializers.ModelSerializer):
+    ingredients = RecipeIngredientSerializer(source='recipeingredient_set', many=True)
+
     class Meta:
         model = Recipe
         fields = '__all__'
