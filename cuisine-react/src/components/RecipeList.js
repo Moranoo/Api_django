d93f6b6ff9f7e950bc68a228ed68789783f8fac9
DavalEnzo <davalenzo@gmail.com>
2024-06-13 14:47:33 +0200

    résolution RecipeList.js manque fermeture

--- cuisine-react/src/components/RecipeList.js
+++ cuisine-react/src/components/RecipeList.js
@@ -100,6 +100,7 @@ function RecipeList() {
             <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6">
                 {recipes.map(recipe => (
                     <RecipeCard key={recipe.id} recipe={recipe}/>
+                ))}
             </div>
             {loading && loadingRecipes()}
             <PageIndicator prevPage={prevPage} nextPage={nextPage} fetchPage={fetchRecipes}
